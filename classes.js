4987e9579f55b41bcfe06535be03cf7aa8eb7b62
Morgan Berry <morganlyn.berry@gmail.com>
2022-06-06 11:27:52 -0600

    Refactor CSS, assets, HTML, and JS

--- classes.js
+++ classes.js
@@ -6,39 +6,10 @@ class Color {
   }
 }
 
-// on page load, a new instance of Palette will be created
+
 class Palette {
   constructor(colors, id) {
     this.colors = [];
     this.id = id;
     this.locked = false;
   }
-
-  // replaces unlocked swatches
-  replaceSwatch() {
-    this.colors = '';
-    if (this.locked === false) {
-      return this.colors
-    }
-  }
-
-// lock swatch
-  lockSwatch() {
-    this.locked = true
-  }
-}
-
-
-
-
-
-// Color class will be a random hex code that is a series of numbers 0-9 and A-F
-// This will display as the hex code and swatch color on the site underneath the color swatch with the lock
-// Color will have an event listener so the user can lock in a color selection
-// All colors begin unlocked
-
-// Palette class a new instance of palette will be declared on page load - event listener on page load
-// A new instance will be created and used until the palette is Saved
-// It should have 5 colors
-// The user should be able to lock colors
-// The user should only replace unlocked colors
